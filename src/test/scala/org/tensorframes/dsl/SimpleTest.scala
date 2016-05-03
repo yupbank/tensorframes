c3c27d6e6184edb19cb476f68a073ce9ed775c2c
Timothy Hunter <timhunter@databricks.com>
2016-05-03 08:26:15 -0700

    enabling more tests

--- src/test/scala/org/tensorframes/dsl/SimpleTest.scala
+++ src/test/scala/org/tensorframes/dsl/SimpleTest.scala
@@ -14,24 +14,24 @@ class SimpleTest extends FunSuite with Logging {
     test(s) { withGraph { fun } }
   }
 
-//  testGraph("Simple test") {
-//    val x = tf.constant(3)
-//    compareOutput("tf.constant(3)", x)
-//  }
-//
-//  testGraph("Named constant") {
-//    val x = tf.constant(3) named "x"
-//    compareOutput("tf.constant(3, name='x')", x)
-//  }
-//
-//  testGraph("Two constants with the same name") {
-//    val x = tf.constant(3)
-//    val x1 = tf.constant(3)
-//    compareOutput(
-//      """tf.constant(3)
-//        |tf.constant(3)
-//      """.stripMargin, x, x1)
-//  }
+  testGraph("Simple test") {
+    val x = tf.constant(3)
+    compareOutput("tf.constant(3)", x)
+  }
+
+  testGraph("Named constant") {
+    val x = tf.constant(3) named "x"
+    compareOutput("tf.constant(3, name='x')", x)
+  }
+
+  testGraph("Two constants with the same name") {
+    val x = tf.constant(3)
+    val x1 = tf.constant(3)
+    compareOutput(
+      """tf.constant(3)
+        |tf.constant(3)
+      """.stripMargin, x, x1)
+  }
 
   testGraph("fill 1") {
     compareOutput("tf.fill([1],1.0)", tf.fill(Seq(1), 1.0))
