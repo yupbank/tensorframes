0407387e9b07e022b7cdfc2c4d73571e415c6ed7
Peng Yu <peng.yu@shopify.com>
2017-05-11 22:25:12 -0400

    add test case for map_blocks

--- src/main/python/tensorframes/core_test.py
+++ src/main/python/tensorframes/core_test.py
@@ -50,6 +50,19 @@ class TestCore(object):
         data2 = df2.collect()
         assert data2[0].z == 3.0, data2
 
+    def test_map_blocks_2(self):
+        data = [dict(x=float(x)) for x in range(10)]
+        df = pd.DataFrame(data)
+        with tf.Graph().as_default() as g:
+            # The placeholder that corresponds to column 'x'
+            x = tf.placeholder(tf.double, shape=[None], name="x")
+            # The output that adds 3 to x
+            z = tf.add(x, 3, name='z')
+            # The resulting dataframe
+            df2 = tfs.map_blocks(z, df)
+        data2 = df2
+        assert data2.z[0] == 3.0, data2
+
     def test_map_rows_1(self):
         data = [Row(x=float(x)) for x in range(5)]
         df = self.sql.createDataFrame(data)
