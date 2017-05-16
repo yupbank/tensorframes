dfe5729f466483a35107499bb0ae40e8ee0dccd7
Peng Yu <yupbank@users.noreply.github.com>
2017-05-16 10:15:51 -0400

    [32] add map_row and map_blocks for pandas dataframe (#105)
    * add _map_pd for pandas dataframe
    
    * add test case for map_rows
    
    * add _map_pd for pandas dataframe
    
    * add pandas requirements
    
    * fix fix fix
    
    * add test case for map_blocks
    
    * update documents

--- src/main/python/tensorframes/core_test.py
+++ src/main/python/tensorframes/core_test.py
@@ -5,6 +5,7 @@ from pyspark import SparkContext
 from pyspark.sql import DataFrame, SQLContext
 from pyspark.sql import Row
 import tensorflow as tf
+import pandas as pd
 
 import tensorframes as tfs
 from tensorframes.core import _java_api
@@ -49,6 +50,19 @@ class TestCore(object):
         data2 = df2.collect()
         assert data2[0].z == 3.0, data2
 
+    def test_map_blocks_2(self):
+        data = [dict(x=float(x)) for x in range(10)]
+        df = pd.DataFrame(data)
+        with tf.Graph().as_default() as g:
+            # The placeholder that corresponds to column 'x'
+            x = tf.placeholder(tf.double, shape=[None], name="x")
+            # The output that adds 3 to x
+            z = tf.add(x, 3, name='z')
+            # The resulting dataframe
+            df2 = tfs.map_blocks(z, df)
+        data2 = df2
+        assert data2.z[0] == 3.0, data2
+
     def test_map_rows_1(self):
         data = [Row(x=float(x)) for x in range(5)]
         df = self.sql.createDataFrame(data)
@@ -75,6 +89,31 @@ class TestCore(object):
         data2 = df2.collect()
         assert data2[0].z == 3.0, data2
 
+    def test_map_rows_3(self):
+        data = [dict(x=float(x)) for x in range(5)]
+        df = pd.DataFrame(data)
+        with tf.Graph().as_default() as g:
+            # The placeholder that corresponds to column 'x'
+            x = tf.placeholder(tf.double, shape=[None], name="x")
+            # The output that adds 3 to x
+            z = tf.add(x, 3, name='z')
+            # The resulting dataframe
+            df2 = tfs.map_rows(z, df)
+        data2 = df2
+        assert data2.z[0] == 3.0, data2
+
+    def test_map_rows_4(self):
+        data = [dict(y=float(x)) for x in range(5)]
+        df = pd.DataFrame(data)
+        with tf.Graph().as_default() as g:
+            # The placeholder that corresponds to column 'x'
+            x = tf.placeholder(tf.double, shape=[None], name="x")
+            # The output that adds 3 to x
+            z = tf.add(x, 3, name='z')
+            # The resulting dataframe
+            df2 = tfs.map_rows(z, df, feed_dict={'x':'y'})
+        data2 = df2
+        assert data2.z[0] == 3.0, data2
 
     def test_reduce_rows_1(self):
         data = [Row(x=float(x)) for x in range(5)]
