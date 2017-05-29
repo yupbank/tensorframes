47b8b780f7b123c76c8d8dee4f12873856d6a74d
Peng Yu <yupbank@users.noreply.github.com>
2017-05-28 20:42:03 -0400

    [106] Add variable support (#107)
    * add variable support
    
    * have to add another option for initial_values
    
    * address the comments

--- src/main/python/tensorframes/core_test.py
+++ src/main/python/tensorframes/core_test.py
@@ -37,6 +37,20 @@ class TestCore(object):
         df = self.sql.createDataFrame(data)
         tfs.print_schema(df)
 
+    def test_map_blocks_0(self):
+        data = [Row(x=float(x)) for x in range(10)]
+        df = self.sql.createDataFrame(data)
+        with tf.Graph().as_default() as g:
+            # The placeholder that corresponds to column 'x'
+            x = tf.placeholder(tf.double, shape=[None], name="x")
+            # The output that adds 3 to x
+            y = tf.Variable(3.0, dtype=tf.double, name='y')
+            z = tf.add(x, y, name='z')
+            # The resulting dataframe
+            df2 = tfs.map_blocks(z, df)
+        data2 = df2.collect()
+        assert data2[0].z == 3.0, data2
+
     def test_map_blocks_1(self):
         data = [Row(x=float(x)) for x in range(10)]
         df = self.sql.createDataFrame(data)
@@ -58,6 +72,20 @@ class TestCore(object):
             x = tf.placeholder(tf.double, shape=[None], name="x")
             # The output that adds 3 to x
             z = tf.add(x, 3, name='z')
+            # The resulting dataframe
+            df2 = tfs.map_blocks(z, df)
+        data2 = df2
+        assert data2.z[0] == 3.0, data2
+
+    def test_map_blocks_3(self):
+        data = [dict(x=float(x)) for x in range(10)]
+        df = pd.DataFrame(data)
+        with tf.Graph().as_default() as g:
+            # The placeholder that corresponds to column 'x'
+            x = tf.placeholder(tf.double, shape=[None], name="x")
+            # The output that adds 3 to x
+            y = tf.Variable(3.0, dtype=tf.double, name='y')
+            z = tf.add(x, y, name='z')
             # The resulting dataframe
             df2 = tfs.map_blocks(z, df)
         data2 = df2
@@ -115,6 +143,21 @@ class TestCore(object):
         data2 = df2
         assert data2.z[0] == 3.0, data2
 
+    def test_reduce_rows_0(self):
+        data = [Row(x=float(x)) for x in range(5)]
+        df = self.sql.createDataFrame(data)
+        with tf.Graph().as_default() as g:
+            # The placeholder that corresponds to column 'x'
+            x_1 = tf.placeholder(tf.double, shape=[], name="x_1")
+            x_2 = tf.placeholder(tf.double, shape=[], name="x_2")
+            y = tf.Variable(0.0, dtype=tf.double, name='y')
+            x_0 = tf.add(y, x_1, name='x_0')
+            # The output that adds 3 to x
+            x = tf.add(x_0, x_2, name='x')
+            # The resulting number
+            res = tfs.reduce_rows(x, df)
+        assert res == sum([r.x for r in data])
+
     def test_reduce_rows_1(self):
         data = [Row(x=float(x)) for x in range(5)]
         df = self.sql.createDataFrame(data)
@@ -140,6 +183,19 @@ class TestCore(object):
             # The resulting dataframe
             res = tfs.reduce_blocks(x, df)
         assert res == sum([r.x for r in data])
+
+    def test_map_blocks_trimmed_0(self):
+        data = [Row(x=float(x)) for x in range(3)]
+        df = self.sql.createDataFrame(data)
+        with tf.Graph().as_default() as g:
+            # The placeholder that corresponds to column 'x'
+            x = tf.placeholder(tf.double, shape=[None], name="x")
+            # The output discards the input and return a single row of data
+            z = tf.Variable([2], dtype=tf.double, name='z')
+            # The resulting dataframe
+            df2 = tfs.map_blocks(z, df, trim=True)
+        data2 = df2.collect()
+        assert data2[0].z == 2, data2
 
     def test_map_blocks_trimmed_1(self):
         data = [Row(x=float(x)) for x in range(3)]
