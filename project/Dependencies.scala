e390e28446563c64f920661e6d275f756ff03ebd
Xiangrui Meng <meng@databricks.com>
2018-08-20 18:22:26 -0700

    use the final 1.10.0 java API release (#155)
    it is now released on maven central

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,5 @@
 object Dependencies {
   // The spark version
   val targetSparkVersion = sys.props.getOrElse("spark.version", "2.3.1")
-  val targetTensorFlowVersion = "1.10.0-rc1"
+  val targetTensorFlowVersion = "1.10.0"
 }
